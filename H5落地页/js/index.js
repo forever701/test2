9399a65295fdd29a4a144440c5789b38cbbabe69
litchi <Caesar@gmail.com>
2023-01-16 11:19:29 +0400

    落地页修改

--- H5落地页/js/index.js
+++ H5落地页/js/index.js
@@ -1,3 +1,5 @@
+
+
 (function flexible(window, document) {
     function resetFontSize() {
         const size = (document.documentElement.clientWidth / 750) * 100;
@@ -7,13 +9,40 @@
     window.addEventListener("resize", resetFontSize);
 })(window, document);
 
- const judgeClient = function () {
+function judgeClient() {
     let u = navigator.userAgent
     let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1 //判断是否是 android终端
     let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) //判断是否是 iOS终端
-    if (isAndroid && window.Android) {
+    if (isAndroid) {
       return 'Android'
-    } else if (isIOS && window.webkit) {
+    } else if (isIOS) {
       return 'IOS'
-    } 
+    }
+}
+
+
+// 解密
+const key = CryptoJS.enc.Utf8.parse('6691e83adhf882rd')
+const iv = CryptoJS.enc.Utf8.parse('6641c83ad8f882rd')
+
+// aes解密
+function decrypt(word) {
+    const encryptedHexStr = CryptoJS.enc.Hex.parse(word)
+    const srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr)
+    const decrypt = CryptoJS.AES.decrypt(srcs, key, {
+        iv: iv,
+        // mode: CryptoJS.mode.CTR,
+        padding: CryptoJS.pad.Pkcs7,
+    })
+    const decryptedStr = decrypt.toString(CryptoJS.enc.Utf8)
+    return JSON.parse(decryptedStr.toString())
+}
+
+function downFile(url) {
+    const el = document.createElement('a');
+    el.href = url;
+    el.setAttribute('download', '可选，下载文件的名字');
+    document.body.appendChild(el);
+    el.click();
+    document.body.removeChild(el);
 }
