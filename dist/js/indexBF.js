75a1de9675a6e48a5e7050b8eb73677e6922cb71
empty <empty@qq.com>
2024-01-15 22:21:29 +0800

    添加 openinstall 相关功能代码

--- dist/js/indexBF.js
+++ dist/js/indexBF.js
@@ -50,99 +50,20 @@ new Vue({
 
         ],
         serviceCustomerUrl: 'https://chat.ichatlink.net/widget/standalone.html?eid=b23dea97e470ffde786ebd78505ae5e1&agentid=80bc82ba885fa28bf3c32cbb390058e5&language=vi',
+        channelCode: '',
     },
     updated() {
        
          
     },
     mounted() {
-        this.reg = window.location.href.split("/")[3]
-        if (this.reg.includes('?')) {
-            this.reg = this.reg.split("?")[0]
-        }
-        
-        // if (this.reg && this.reg.length >= 7) {
-        //     this.lang = this.langs[this.reg.slice(0, -6)] || 'zh';
-        // } else {
-        //     this.lang = getCurrentLanguage()[0]
-        // }
-        this.sideCode = this.lang === 'zh' ? 0 : 1;
-        this.newDate = +new Date()
+       
         document.title = this.allData[this.lang].appName
         this.handleIcoCreate(`./img/logo-icon-${this.lang}.png`);
-        const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
-            .then(FingerprintJS => FingerprintJS.load())
-        fpPromise
-            .then(fp => fp.get())
-            .then(result => {
-                this.udid = result.visitorId.toUpperCase()
-                this.getDownloadList();
-                console.log(this.udid)
-            });
-        this.bindSwiper()
+        this.getQueryStringByName("channelCode")
     },
     methods: {
-        bindSwiper() {
-            var mySwiper = new Swiper (document.getElementById("swiper-container"), {
-                loop: true,
-                autoplay: true,
-                speed:3000,
-                nextButton: 'null',
-                prevButton: 'null',
-                slidesPerView : "auto",
-                observer:true,
-                observeParents:true,
-
-              });     
-        },
-        getDownloadList() {
-            axios.post(this.apiUrl + `/promotion/app/user/landingpage/list`, { os: judgeClient() === 'IOS' ? 2 : 1, invitationCode: this.reg }, {
-                headers: {
-                    'Content-type': 'application/json',
-                    'X-UDID': this.udid,
-                    'X-Timestamp': this.newDate,
-                    'X-Sign': $.md5(this.udid + '6fff,3344322' + this.newDate),
-                    'side_code': 1
-                }
-            }).then((res) => {
-                const response = res.data
-                if (response.secret) {
-                    response.data = decrypt(response.data)
-                }
-                this.ios = response.data.filter(item => item.position === 4)[0]
-                this.android = response.data.filter(item => item.position === 1)[0]
-                this.h5 = response.data.filter(item => item.position === 2)[0]
-                this.serviceUrl = response.data.filter(item => item.position === 5)[0].urlDown
-                this.downloadList = response.data.filter(item => item.position === 3)
-            })
-        },
-        onDownloadApp() {
-            if (judgeClient() === 'IOS') {
-                this.downloadApp(this.ios)
-            } else {
-                this.downloadApp(this.android)
-            }
-        },
-        downloadApp(item) {
-            this.addInvitationCodeClip();
-            axios.get(item.urlDown + this.reg, {
-                headers: {
-                    'Content-type': 'application/json',
-                    'X-UDID': this.udid,
-                    'X-Timestamp': this.newDate,
-                    'X-Sign': $.md5(this.udid + '6fff,3344322' + this.newDate),
-                    'side_code': 1
-                }
-            }).then(res => {
-                let response = res.data;
-                if (response.secret) {
-                    response.data = decrypt(response.data)
-                }
-                if (response.data) {
-                    location.href = response.data;
-                }
-            })
-        },
+        
         // 添加邀请码到剪切板
         addInvitationCodeClip() {
             // 创建一个隐藏的文本输入框元素
@@ -166,12 +87,16 @@ new Vue({
         },
         buttonClick(index) {
             if (index < 4) {
-                window.open("https://" + this.urlList[index])
-            } else if(index == 4) {
-                this.downloadApp(this.ios)
-            } else {
-                this.downloadApp(this.android)
+                window.open("https://" + this.urlList[index] + "/?channelCode="+this.channelCode)
             }
+        },
+        //根据参数名获取参数值
+        getQueryStringByName(name){
+            var result = location.search.match(new RegExp("[\?\&]" + name+ "=([^\&]+)","i"));
+            if(result != null && result.length > 0){
+                this.channelCode = result[1]
+            }
+            
         },
         handleIcoCreate(icoUrl) {
             var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
