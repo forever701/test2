e41a53e5bab8fff6fc852a72439ed8ead97958f2
empty <empty@qq.com>
2024-01-15 13:45:32 +0800

    去掉无用代码

--- dist/js/indexBf.js
+++ dist/js/indexBf.js
@@ -42,10 +42,7 @@ new Vue({
         }
     },
     mounted() {
-        this.reg = window.location.href.split("/")[3]
-        if (this.reg.includes('?')) {
-            this.reg = this.reg.split("?")[0]
-        }
+
         this.domain = window.location.protocol + "//" + window.location.host;
 
         // if (this.reg && this.reg.length >= 7) {
@@ -57,86 +54,9 @@ new Vue({
         this.newDate = +new Date()
         document.title = this.allData[this.lang].appName
         this.handleIcoCreate(`./img/logo-icon-${this.lang}.png`);
-        const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
-            .then(FingerprintJS => FingerprintJS.load())
-        fpPromise
-            .then(fp => fp.get())
-            .then(result => {
-                this.udid = result.visitorId.toUpperCase()
-                this.getDownloadList();
-                console.log(this.udid)
-            })
-        //var button = document.getElementsByName("downloadButton");
+
     },
     methods: {
-
-        openInstallApk() {
-            var data = OpenInstall.parseUrlParams();///openinstall.js中提供的api，解析当前网页url中的查询参数并对data进行赋值
-            new OpenInstall({//初始化方法，与openinstall服务器交互，应尽早调用
-                appKey : "mslhmh",//appKey为openinstall为应用分配的唯一id（必须传入）
-                onready : function() {//初始化成功回调方法。当初始化完成后，会自动进入
-                    this.schemeWakeup();//尝试使用scheme打开App（主要用于Android以及iOS的QQ环境中）
-                    var m = this, button = document.getElementByName("downloadButton");//为button对象绑定对应id的元素
-                    button.onclick = function() {//对应button的点击事件
-                        m.wakeupOrInstall();//此方法为scheme、Universal Link唤醒以及引导下载的作用（必须调用且不可额外自行跳转下载）
-                        return false;
-                    }
-                }
-            }, data);//初始化时传入data，作为一键拉起/App传参安装时候的参数
-        },
-
-        getDownloadList() {
-            let os = judgeClient() === 'IOS' ? 2 : 1
-            axios.post(this.apiUrl + `/promotion/app/user/landingpage/list`, { 'os': os, 'invitationCode': this.reg, 'url': this.domain }, {
-                headers: {
-                    'Content-type': 'application/json',
-                    'X-UDID': this.udid,
-                    'X-Timestamp': this.newDate,
-                    'X-Sign': $.md5(this.udid + 'jgyh,kasd' + this.newDate),
-                    'side_code': 1
-                }
-            }).then((res) => {
-                const response = res.data
-                if (response.secret) {
-                    response.data = decrypt(response.data)
-                }
-                this.ios = response.data.filter(item => item.position === 4)[0]
-                this.android = response.data.filter(item => item.position === 1)[0]
-                this.h5 = response.data.filter(item => item.position === 2)[0]
-                this.serviceUrl = response.data.filter(item => item.position === 5)[0]?.urlDown
-                this.downloadList = response.data.filter(item => item.position === 3)
-            })
-        },
-        onDownloadApp() {
-            if (judgeClient() === 'IOS') {
-                this.downloadApp(this.ios)
-            } else {
-                this.downloadApp(this.android)
-            }
-        },
-        downloadApp(item) {
-            const url  = `&url=${this.domain}`
-            axios.get(item.urlDown + this.reg + url, {
-                headers: {
-                    'Content-type': 'application/json',
-                    'X-UDID': this.udid,
-                    'X-Timestamp': this.newDate,
-                    'X-Sign': $.md5(this.udid + 'jgyh,kasd' + this.newDate),
-                    'side_code': 1
-                }
-            }).then(res => {
-                let response = res.data;
-                if (response.secret) {
-                    response.data = decrypt(response.data)
-                }
-                if (response.data) {
-                    location.href = response.data;
-                }
-            })
-        },
-        onService() {
-            window.open(this.serviceUrl)
-        },
         handleIcoCreate(icoUrl) {
             var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
             link.type = 'image/x-icon';
